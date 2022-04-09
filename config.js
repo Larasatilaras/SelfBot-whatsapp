e5d2d9412be84a8ed5cdad6615d1028cd305d414
Laras <96417338+Larasatilaras@users.noreply.github.com>
2022-04-09 11:34:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ let fs = require('fs');
 let chalk = require('chalk');
 
 //Nomer Owner
-global.owner = ['6285890441472'] // Put your number here
+global.owner = ['6285759662798'] // Put your number here
 
 // Petunjuk Di Menu
 global.inUrl = '<link>'
@@ -32,9 +32,9 @@ const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
   var sticker_name = `𝙵ͥ͢𝚊ͣ𝚛ͫ𝚒͓͒𝚒𝙳͓ͯ - 💓
 
-❑ ιηѕтαgяαм : @faridmhrdkaa
-❑ gιтнυв : @Fariiid-M
-❑ ωнαтѕαρρ : 6285890441472\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n`
+❑ ιηѕтαgяαм : @itzmelaras_
+❑ gιтнυв : @https://github.com/Larasatilaras
+❑ ωнαтѕαρρ : 6285759662798\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n`
   var sticker_author = ``
 } else {
   var sticker_name = stickerpack.spackname
